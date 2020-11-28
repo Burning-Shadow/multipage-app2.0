0582d38b778f1cf702711ed3850777b8b4a59ad1
Burning-Shadow <2485999859@qq.com>
2020-11-28 21:34:22 +0800

    feat: 修复home页路径匹配问题

--- src/pages/index/router.js
+++ src/pages/index/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -18,8 +17,8 @@ export default new Router({
     {
       name: 'home',
       path: '/home',
-      component: Home,
-      alias: '/home.html',
+      alias: '/home/*',
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue'),
       meta: {
         pageUrl: './home.html',
         title: 'Home Page',
